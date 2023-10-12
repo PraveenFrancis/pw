9e174a1e51b3d467de3e70bb4b2d601f9fd7b0a9
PraveenFrancis <praveenfrancis555@gmail.com>
2023-10-13 00:51:30 +0530

    Added my c

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 self.addEventListener("push", function (event) {
     console.log(event)
-  let data = event.data.json();
+//   let data = event.data.json();
   const image =
     "https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230";
   const options = {
-    body: data.options.body,
+    body: 'data.options.body',
     icon: image,
   };
 //   self.registration.showNotification(data.title, options);
