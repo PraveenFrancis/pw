fe45780a66be57d4307f984f1cfbf9f7e77c1670
PraveenFrancis <praveenfrancis555@gmail.com>
2023-10-13 00:48:29 +0530

    Added my projectffssd

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 self.addEventListener("push", function (event) {
+    console.log(event)
   let data = event.data.json();
   const image =
     "https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230";
@@ -6,7 +7,7 @@ self.addEventListener("push", function (event) {
     body: data.options.body,
     icon: image,
   };
-  self.registration.showNotification(data.title, options);
+//   self.registration.showNotification(data.title, options);
   //    const options = {}
   event.waitUntil(self.registration.showNotification("Hello workd!", options));
 });
