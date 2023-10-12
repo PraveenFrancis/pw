6cbaed3d4ed00be0cb81afa16966b9783df5d681
PraveenFrancis <praveenfrancis555@gmail.com>
2023-10-13 00:42:28 +0530

    Added my projectff

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-self.addEventListener('push', function(e) {
-   const options = {}
-   e.waitUntil(self.registration.showNotification('Hello workd!', options))
+self.addEventListener("push", function (event) {
+  let data = event.data.json();
+  const image =
+    "https://cdn.glitch.com/614286c9-b4fc-4303-a6a9-a4cef0601b74%2Flogo.png?v=1605150951230";
+  const options = {
+    body: data.options.body,
+    icon: image,
+  };
+  self.registration.showNotification(data.title, options);
+  //    const options = {}
+  e.waitUntil(self.registration.showNotification("Hello workd!", options));
 });
