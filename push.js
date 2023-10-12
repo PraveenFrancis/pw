881d4e90ab2323d82af77c9e257f04b75c12edc8
PraveenFrancis <praveenfrancis555@gmail.com>
2023-10-12 23:45:49 +0530

    inital

---
+++ push.js
@@ -0,0 +1,11 @@
+const push = require('web-push');
+
+let vapidkeys = {
+    publicKey: 'BPDkpu94JBfU8g1gj9d6zbrTQtf_TL9uCFh8iO_xmQy_QCGGMNucyPW16PlGzgm7Z9jnvui4qG4WsVv8DO33FPk',
+    privateKey: 'mwxDPEVsQmz0CrI5MQig80JZaM7xhbVkysBiOLNNitI'
+  }
+
+push.setVapidDetails('mailto:238773@ust-global.com', vapidkeys.privateKey, vapidkeys.privateKey)
+
+let sub = {};
+push.sendNotification(sub, 'test message')
